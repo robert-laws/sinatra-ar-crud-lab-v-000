a5110ae622a1e84eb3e822a3a1940095a0d13285
Robert Laws <roblaws@me.com>
2017-10-27 14:18:37 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  post '/posts' do
+    Post.create(name: params[:name], content: params[:content])
+    @posts = Post.all
+    erb :index
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    post = Post.find(params[:id])
+    post.update(name: params[:name], content: params[:content])
+  end
+
+  delete '/posts/:id/delete' do
+    post = Post.find(params[:id])
+    post.destroy
   end
 end
